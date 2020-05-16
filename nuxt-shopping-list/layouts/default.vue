35d18ba0da3443afb36dbb8d81194c2f17066806
Alex Bowers <alex.bowers@bluebaytravel.co.uk>
2020-05-16 13:38:21 +0100

    Set more body styles

--- nuxt-shopping-list/layouts/default.vue
+++ nuxt-shopping-list/layouts/default.vue
@@ -17,7 +17,11 @@ export default {
 
 <style lang="scss">
 body {
+    background-color: $blue-4;
+    color: $blue-19;
+    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    font-size: 16px;
     margin: 0;
-    background-color: $city-blue;
 }
 </style>
