14dc4fcf9c763c6ee6bd91ac0aa258b360600d36
Alex Bowers <alex.bowers@bluebaytravel.co.uk>
2020-05-23 13:31:10 +0100

    Set title and description though .env

--- nuxt-shopping-list/nuxt.config.js
+++ nuxt-shopping-list/nuxt.config.js
@@ -1,6 +1,4 @@
-const pkg = require('./package')
 const env = require('dotenv').config()
-
 
 module.exports = {
     mode: 'universal',
@@ -14,11 +12,14 @@ module.exports = {
     ** Headers of the page
     */
     head: {
-        title: pkg.name,
+        htmlAttrs: {
+            lang: 'en'
+        },
+        title: process.env.TITLE || '',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: pkg.description }
+            { hid: 'description', name: 'description', content: process.env.DESCRIPTION || '' }
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
